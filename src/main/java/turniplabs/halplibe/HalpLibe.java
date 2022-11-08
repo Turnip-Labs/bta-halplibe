9b05de03f179c2a3cfe46295f2c25b642738786b
azurelmao <de_arcane@protonmail.com>
2022-11-08 21:09:19 +0100

    Fetch client jar from Mojang, remove test

--- src/main/java/turniplabs/halplibe/HalpLibe.java
+++ src/main/java/turniplabs/halplibe/HalpLibe.java
@@ -1,10 +1,8 @@
 package turniplabs.halplibe;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.src.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.TextureHelper;
 
 public class HalpLibe implements ModInitializer {
     public static final String MOD_ID = "halplibe";
@@ -14,11 +12,8 @@ public class HalpLibe implements ModInitializer {
         return MOD_ID + "." + name;
     }
 
-    public static final Item testItem = new Item(200).setItemName("testitem");
-
     @Override
     public void onInitialize() {
         LOGGER.info("HalpLibe initialized.");
-        TextureHelper.addTextureToItems(MOD_ID, "test.png", 16, 0);
     }
 }
