e66f7f1eb4f0d3283141bbf0618cb6bf9d559bd9
azurelmao <de_arcane@protonmail.com>
2022-11-11 18:29:53 +0100

    Why didn't I think of this

--- src/main/java/turniplabs/halplibe/helper/BlockHelper.java
+++ src/main/java/turniplabs/halplibe/helper/BlockHelper.java
@@ -1,16 +1,14 @@
 package turniplabs.halplibe.helper;
 
-import turniplabs.halplibe.mixin.helper.BlockInterface;
 import net.minecraft.src.Block;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemBlock;
-import net.minecraft.src.Material;
 import net.minecraft.src.StepSound;
+import turniplabs.halplibe.mixin.helper.BlockInterface;
 
 public class BlockHelper {
 
-    public static Block createBlock(int id, String name, int x, int y, Material material, StepSound stepSound, float hardness, float resistance, float lightValue) {
-        Block block = new Block(id, material);
+    public static Block createBlock(Block block, String name, int x, int y, StepSound stepSound, float hardness, float resistance, float lightValue) {
         block.setTexCoords(x, y);
         block.setBlockName(name);
 
@@ -24,8 +22,21 @@ public class BlockHelper {
         return block;
     }
 
-    public static Block createBlock(int id, String name, int topX, int topY, int bottomX, int bottomY, int sidesX, int sidesY, Material material, StepSound stepSound, float hardness, float resistance, float lightValue) {
-        Block block = new Block(id, material);
+    public static Block createBlock(Block block, String name, int topBottomX, int topBottomY, int sidesX, int sidesY, StepSound stepSound, float hardness, float resistance, float lightValue) {
+        block.setTexCoords(topBottomX, topBottomY, sidesX, sidesY);
+        block.setBlockName(name);
+
+        ((BlockInterface) block).callSetHardness(hardness);
+        ((BlockInterface) block).callSetResistance(resistance);
+        ((BlockInterface) block).callSetStepSound(stepSound);
+        ((BlockInterface) block).callSetLightValue(lightValue);
+
+        Item.itemsList[block.blockID] = new ItemBlock(block.blockID - Block.blocksList.length);
+
+        return block;
+    }
+
+    public static Block createBlock(Block block, String name, int topX, int topY, int bottomX, int bottomY, int sidesX, int sidesY, StepSound stepSound, float hardness, float resistance, float lightValue) {
         block.setTexCoords(topX, topY, bottomX, bottomY, sidesX, sidesY);
         block.setBlockName(name);
 
@@ -39,8 +50,7 @@ public class BlockHelper {
         return block;
     }
 
-    public static Block createBlock(int id, String name, int topX, int topY, int bottomX, int bottomY, int northX, int northY, int eastX, int eastY, int southX, int southY, int westX, int westY, Material material, StepSound stepSound, float hardness, float resistance, float lightValue) {
-        Block block = new Block(id, material);
+    public static Block createBlock(Block block, String name, int topX, int topY, int bottomX, int bottomY, int northX, int northY, int eastX, int eastY, int southX, int southY, int westX, int westY, StepSound stepSound, float hardness, float resistance, float lightValue) {
         block.setTexCoords(topX, topY, bottomX, bottomY, northX, northY, eastX, eastY, southX, southY, westX, westY);
         block.setBlockName(name);
 
