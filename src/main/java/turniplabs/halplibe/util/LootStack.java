2538349f2324157e7b4b7b4d1969ce105bac5f9a
Oly <oly@yourmoms.house>
2022-11-22 01:10:37 -0300

    refactored lootStack

--- src/main/java/turniplabs/halplibe/util/LootStack.java
+++ src/main/java/turniplabs/halplibe/util/LootStack.java
@@ -1,75 +1,96 @@
 package turniplabs.halplibe.util;
 
+import net.minecraft.src.Block;
+import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 
 import java.util.Random;
 
 public final class LootStack {
 
-    public ItemStack itemStack;
+    public Item item;
     // the actual item
 
     public int rarity;
     // how rare it is, 99 equals a one hundred in one chance of finding it etc.
 
     public int maxQuantity;
-    // do NOT put anything under 1.
+    public int minQuantity;
+    public int maxMetadata;
+    public int minMetadata;
 
-    public int metadataRange;
     // the range of damage values a given item can spawn with.
-    // NOTE: the resulting value is added to the original damage value, not overriding it.
 
 
-    public LootStack(ItemStack itemStack, int rarity, int maxQuantity, int metadataRange) {
-        this.itemStack = itemStack;
+    public LootStack(Item item, int rarity, int maxQuantity, int minQuantity, int maxMetadata, int minMetadata) {
+        this.item = item;
         this.rarity = rarity;
         this.maxQuantity = maxQuantity;
-        this.metadataRange = metadataRange;
+        this.minQuantity = minQuantity;
+        this.maxMetadata = maxMetadata;
+        this.minMetadata = minMetadata;
     }
 
-    public LootStack(ItemStack itemStack){
-        this(itemStack, 0, 1,0);
+    public LootStack(Block block, int rarity, int maxQuantity, int minQuantity, int maxMetadata, int minMetadata) {
+        this.item = new ItemStack(block).getItem();
+        this.rarity = rarity;
+        this.maxQuantity = maxQuantity;
+        this.minQuantity = minQuantity;
+        this.maxMetadata = maxMetadata;
+        this.minMetadata = minMetadata;
     }
 
-    public LootStack(ItemStack itemStack,int rarity){
-        this(itemStack, rarity, 1,0);
+    public LootStack(Item item){
+        this(item, 0, 1, 1,0, 0);
     }
 
-    public LootStack(ItemStack itemStack,int rarity, int maxQuantity){
-        this(itemStack, rarity, maxQuantity,0);
+    public LootStack(Item item,int rarity){
+        this(item, rarity, 1, 1,0, 0);
+    }
+
+    public LootStack(Item item,int rarity, int maxQuantity){
+        this(item, rarity, maxQuantity, 1, 0, 0);
+    }
+
+    public LootStack(Block block, int rarity, int maxQuantity){
+        this( block, rarity, maxQuantity, 1, 0, 0);
+    }
+
+    public LootStack(Item item,int rarity, int maxQuantity, int minQuantity){
+        this(item, rarity, maxQuantity, minQuantity, 0, 0);
     }
 
     private final Random random = new Random();
     public ItemStack generateLoot() {
-        ItemStack outStack;
+        int quantity = 1;
+        int metadata;
 
         if ( this.rarity != 0) {
 
             if (random.nextInt(rarity) == 0) {
-                outStack = new ItemStack(this.itemStack.getItem());
 
-                if (this.maxQuantity > 1)
-                    outStack.stackSize = 1 + random.nextInt(this.maxQuantity);
+                if (this.maxQuantity - minQuantity > 1)
+                    quantity = random.nextInt(this.maxQuantity - this.minQuantity) + this.minQuantity;
 
-                if (this.metadataRange > 0)
-                    outStack.setMetadata( random.nextInt(this.metadataRange) );
+                if (this.maxMetadata - minMetadata > 0)
+                    metadata = random.nextInt( this.maxMetadata - this.minMetadata) + this.minMetadata;
+                    else metadata = this.minMetadata;
 
-                } else outStack = null;
+                } else return null;
 
         } else {
 
             // if rarity equals to zero.
-            outStack = this.itemStack;
+            if (this.maxQuantity - minQuantity > 1)
+                quantity = random.nextInt(this.maxQuantity - this.minQuantity) + this.minQuantity;
 
-            if (this.maxQuantity > 1)
-                outStack.stackSize = 1 + random.nextInt(this.maxQuantity);
+            if (this.maxMetadata - minMetadata > 0)
+                metadata = random.nextInt( this.maxMetadata - this.minMetadata) + this.minMetadata;
+                else metadata = this.minMetadata;
 
-            if (this.metadataRange > 0)
-                outStack.setMetadata( random.nextInt(this.metadataRange) );
+        }
 
-            }
-
-        return  outStack;
+        return  new ItemStack(this.item, quantity, metadata);
     }
 
 }
--- src/main/java/turniplabs/halplibe/util/LootTables.java
+++ src/main/java/turniplabs/halplibe/util/LootTables.java
@@ -12,31 +12,31 @@ public class LootTables {
     static {
         dungeonLoot = new HashMap<Integer, LootStack>() {{
 
-            put(0, new LootStack(new ItemStack(Item.saddle)));
-            put(1, new LootStack(new ItemStack(Item.ingotIron), 0, 5));
-            put(2, new LootStack(new ItemStack(Item.foodBread)));
-            put(3, new LootStack(new ItemStack(Item.wheat), 0, 5));
-            put(4, new LootStack(new ItemStack(Item.sulphur), 0, 5));
-            put(5, new LootStack(new ItemStack(Item.string), 0, 5));
-            put(6, new LootStack(new ItemStack(Item.bucket)));
-            put(7, new LootStack(new ItemStack(Item.foodAppleGold), 100));
-            put(8, new LootStack(new ItemStack(Item.dustRedstone), 2, 16));
-            put(9, new LootStack(new ItemStack(Item.foodApple)));
-            put(10, new LootStack(new ItemStack(Item.dye, 1, 3)));
-            put(11, new LootStack(new ItemStack(Block.spongeDry), 0, 5));
-            put(12, new LootStack(new ItemStack(Item.bone), 0, 5));
+            put(0, new LootStack(Item.saddle) );
+            put(1, new LootStack(Item.ingotIron, 0, 5, 1) );
+            put(2, new LootStack(Item.foodBread) );
+            put(3, new LootStack(Item.wheat, 0, 5) );
+            put(4, new LootStack(Item.sulphur, 0, 5) );
+            put(5, new LootStack(Item.string, 0, 5) );
+            put(6, new LootStack(Item.bucket) );
+            put(7, new LootStack(Item.foodAppleGold, 100) );
+            put(8, new LootStack(Item.dustRedstone, 2, 6,3) );
+            put(9, new LootStack(Item.foodApple) );
+            put(10, new LootStack(Item.dye, 1, 1,1, 3,3) );
+            put(11, new LootStack( Block.spongeDry, 0, 5) );
+            put(12, new LootStack(Item.bone, 0, 5) );
 
             //disks
-            put(13, new LootStack(new ItemStack(Item.record13), 50));
-            put(14, new LootStack(new ItemStack(Item.recordCat), 50));
-            put(15, new LootStack(new ItemStack(Item.recordBlocks), 50));
-            put(16, new LootStack(new ItemStack(Item.recordChirp), 50));
-            put(17, new LootStack(new ItemStack(Item.recordFar), 50));
-            put(18, new LootStack(new ItemStack(Item.recordMall), 50));
-            put(19, new LootStack(new ItemStack(Item.recordMellohi), 50));
-            put(20, new LootStack(new ItemStack(Item.recordStal), 50));
-            put(21, new LootStack(new ItemStack(Item.recordStrad), 50));
-            put(22, new LootStack(new ItemStack(Item.recordWard), 50));
+            put(13, new LootStack(Item.record13, 50) );
+            put(14, new LootStack(Item.recordCat, 50) );
+            put(15, new LootStack(Item.recordBlocks, 50) );
+            put(16, new LootStack(Item.recordChirp, 50) );
+            put(17, new LootStack(Item.recordFar, 50) );
+            put(18, new LootStack(Item.recordMall, 50) );
+            put(19, new LootStack(Item.recordMellohi, 50) );
+            put(20, new LootStack(Item.recordStal, 50) );
+            put(21, new LootStack(Item.recordStrad, 50) );
+            put(22, new LootStack(Item.recordWard, 50) );
 
         }};
     }
@@ -45,33 +45,33 @@ public class LootTables {
 
     static {
         labyrinthLoot = new HashMap<Integer, LootStack>() {{
-            put(0, new LootStack( new ItemStack(Item.ingotIron),0,6) );
-            put(1, new LootStack( new ItemStack(Item.ingotGold),0,4) );
-            put(2, new LootStack( new ItemStack(Item.sulphur,3),0,6) );
-            put(3, new LootStack( new ItemStack(Item.diamond),50,3) );
-            put(4, new LootStack( new ItemStack(Item.foodAppleGold),100) );
-            put(5, new LootStack( new ItemStack(Item.dustRedstone),0,4) );
-            put(6, new LootStack( new ItemStack(Item.foodApple),0) );
-            put(7, new LootStack(new ItemStack(Block.spongeDry), 0, 5) );
-            put(8, new LootStack( new ItemStack(Item.handcannonLoaded), 50));
-            put(9, new LootStack( new ItemStack(Item.handcannonUnloaded), 50));
-            put(10, new LootStack( new ItemStack(Item.armorHelmetChainmail),5,1, 120) );
-            put(11, new LootStack( new ItemStack(Item.armorChestplateChainmail),5,1, 120) );
-            put(12, new LootStack( new ItemStack(Item.armorLeggingsChainmail),5,1, 120) );
-            put(13, new LootStack( new ItemStack(Item.armorBootsChainmail),5,1, 120) );
-            put(14, new LootStack( new ItemStack(Item.ingotSteelCrude),10,3) );
+            put(0, new LootStack( Item.ingotIron,0,6) );
+            put(1, new LootStack( Item.ingotGold,0,4) );
+            put(2, new LootStack( Item.sulphur,0,6) );
+            put(3, new LootStack( Item.diamond,50,6,3) );
+            put(4, new LootStack( Item.foodAppleGold,100) );
+            put(5, new LootStack( Item.dustRedstone,0,4) );
+            put(6, new LootStack( Item.foodApple,0) );
+            put(7, new LootStack( Block.spongeDry, 0, 5) );
+            put(8, new LootStack( Item.handcannonLoaded, 50));
+            put(9, new LootStack( Item.handcannonUnloaded, 50));
+            put(10, new LootStack( Item.armorHelmetChainmail,5,1,1, 240,120) );
+            put(11, new LootStack( Item.armorChestplateChainmail,5,1,1, 240,120) );
+            put(12, new LootStack( Item.armorLeggingsChainmail,5,1, 1, 240,120) );
+            put(13, new LootStack( Item.armorBootsChainmail,5,1, 1, 240,120) );
+            put(14, new LootStack( Item.ingotSteelCrude,10,3) );
 
             //disks
-            put(15, new LootStack(new ItemStack(Item.record13), 50));
-            put(16, new LootStack(new ItemStack(Item.recordCat), 50));
-            put(17, new LootStack(new ItemStack(Item.recordBlocks), 50));
-            put(18, new LootStack(new ItemStack(Item.recordChirp), 50));
-            put(19, new LootStack(new ItemStack(Item.recordFar), 50));
-            put(20, new LootStack(new ItemStack(Item.recordMall), 50));
-            put(21, new LootStack(new ItemStack(Item.recordMellohi), 50));
-            put(22, new LootStack(new ItemStack(Item.recordStal), 50));
-            put(23, new LootStack(new ItemStack(Item.recordStrad), 50));
-            put(24, new LootStack(new ItemStack(Item.recordWard), 50));
+            put(15, new LootStack(Item.record13, 50) );
+            put(16, new LootStack(Item.recordCat, 50) );
+            put(17, new LootStack(Item.recordBlocks, 50) );
+            put(18, new LootStack(Item.recordChirp, 50) );
+            put(19, new LootStack(Item.recordFar, 50) );
+            put(20, new LootStack(Item.recordMall, 50) );
+            put(21, new LootStack(Item.recordMellohi, 50) );
+            put(22, new LootStack(Item.recordStal, 50) );
+            put(23, new LootStack(Item.recordStrad, 50) );
+            put(24, new LootStack(Item.recordWard, 50) );
 
         }};
 }
