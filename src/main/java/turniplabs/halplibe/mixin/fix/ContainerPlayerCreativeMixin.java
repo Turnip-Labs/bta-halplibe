7f374037eaff8837028cabdd449d7c7174fdda36
azurelmao <de_arcane@protonmail.com>
2023-01-10 00:49:09 +0100

    Remove unused imports

--- src/main/java/turniplabs/halplibe/mixin/fix/ContainerPlayerCreativeMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/fix/ContainerPlayerCreativeMixin.java
@@ -4,12 +4,8 @@ import net.minecraft.src.ContainerPlayerCreative;
 import net.minecraft.src.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import turniplabs.halplibe.HalpLibe;
 
 import java.util.Arrays;
 
