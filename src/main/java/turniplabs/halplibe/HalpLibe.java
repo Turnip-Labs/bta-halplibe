acce404c8ee1075fe3d667f6114205cfbd1b13bc
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2023-08-12 12:05:47 +0200

    Possibly fix some class loading issues?

--- src/main/java/turniplabs/halplibe/HalpLibe.java
+++ src/main/java/turniplabs/halplibe/HalpLibe.java
@@ -12,6 +12,14 @@ public class HalpLibe implements ModInitializer {
 		return modId + "." + name;
 	}
 
+	static {
+		//this is here to possibly fix some class loading issues, do not delete
+		try {
+			Class.forName("net.minecraft.core.block.Block");
+			Class.forName("net.minecraft.core.item.Item");
+		} catch (ClassNotFoundException ignored) {}
+	}
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("HalpLibe initialized.");
