26a1c177b905e9fbe82721a0e4ef390a8eaff005
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2023-08-12 13:24:18 +0200

    bruh momento

--- src/main/java/turniplabs/halplibe/HalpLibe.java
+++ src/main/java/turniplabs/halplibe/HalpLibe.java
@@ -1,8 +1,18 @@
 package turniplabs.halplibe;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.impl.FabricLoaderImpl;
+import net.fabricmc.loader.impl.launch.FabricLauncher;
+import net.fabricmc.loader.impl.launch.knot.Knot;
+import net.minecraft.client.render.block.color.BlockColorDispatcher;
+import net.minecraft.client.render.block.model.BlockModel;
+import net.minecraft.client.render.block.model.BlockModelDispatcher;
+import net.minecraft.client.render.block.model.BlockModelRenderBlocks;
+import net.minecraft.core.block.Block;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.mixin.accessors.DispatcherAccessor;
 
 public class HalpLibe implements ModInitializer {
 	public static final String MOD_ID = "halplibe";
@@ -13,7 +23,7 @@ public class HalpLibe implements ModInitializer {
 	}
 
 	static {
-		//this is here to possibly fix some class loading issues, do not delete
+		//this is here to possibly fix some class loading issues but might not work anyway, delete if it causes even more problems
 		try {
 			Class.forName("net.minecraft.core.block.Block");
 			Class.forName("net.minecraft.core.item.Item");
