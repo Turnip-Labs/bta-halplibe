299bbcda9e37dda4fe99f2ca4a526021c99bb142
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2023-09-21 18:32:40 -0400

    forgot to remove my test code, lol

--- src/main/java/turniplabs/halplibe/HalpLibe.java
+++ src/main/java/turniplabs/halplibe/HalpLibe.java
@@ -28,34 +28,6 @@ public class HalpLibe implements ModInitializer {
         } catch (ClassNotFoundException ignored) {
         }
         
-        RegistryHelper.scheduleRegistry(false, () -> {
-            int open = BlockHelper.findOpenIds(20) + 19;
-            int openItem = ItemHelper.findOpenIds(21) + 19;
-            for (int i1 = 0; i1 < 20; i1++) {
-                int i = i1;
-                Block bk = new BlockBuilder("lol" ).noItemBlock().build(new Block("lul" + i, i + open, Material.stone));
-                ItemHelper.createItem(
-                        "lol", new SpecialItemBlock(i + openItem, bk),
-                        "lul" + i
-                );
-            }
-            ItemHelper.createItem(
-                    "lol",
-                    new Item(openItem + 20),
-                    "lul" + 20
-            );
-        });
-        RegistryHelper.scheduleRegistry(false, () -> {
-            int open = BlockHelper.findOpenIds(20);
-            int openItem = ItemHelper.findOpenIds(20);
-            for (int i = 0; i < 20; i++) {
-                Block bk = new BlockBuilder("lol" ).noItemBlock().build(new Block("lul" + (i + 20), i + open, Material.stone));
-                ItemHelper.createItem(
-                        "lol", new SpecialItemBlock(i + openItem, bk),
-                        "lul" + (i + 21)
-                );
-            }
-        });
     }
 
     public static String addModId(String modId, String name) {
