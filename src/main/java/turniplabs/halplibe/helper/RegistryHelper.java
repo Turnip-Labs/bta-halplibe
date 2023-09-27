3c3cbbd6db0c5b7a9cbfeec10657ffb6d908a5c9
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2023-09-27 11:01:27 -0400

    correct "typo"(?)

--- src/main/java/turniplabs/halplibe/helper/RegistryHelper.java
+++ src/main/java/turniplabs/halplibe/helper/RegistryHelper.java
@@ -3,17 +3,17 @@ package turniplabs.halplibe.helper;
 import java.util.ArrayList;
 
 public class RegistryHelper {
-	private static final ArrayList<Runnable> regsiterFunctions = new ArrayList<>();
+	private static final ArrayList<Runnable> regsitryFunctions = new ArrayList<>();
 	
-	public static void scheduleRegistry(boolean configured, Runnable register) {
-		if (configured) regsiterFunctions.add(0, register);
-		else regsiterFunctions.add(register);
+	public static void scheduleRegistry(boolean configured, Runnable function) {
+		if (configured) regsitryFunctions.add(0, function);
+		else regsitryFunctions.add(function);
 	}
 	
 	@SuppressWarnings("unused")
 	private static void runRegistry() {
-		for (Runnable regsiterFunction : regsiterFunctions) {
-			regsiterFunction.run();
+		for (Runnable regsitryFunction : regsitryFunctions) {
+			regsitryFunction.run();
 		}
 	}
 }
