4b9356d04964d5e6194a726817187618d6658131
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-14 09:34:36 -0500

    Stat system now equivalent in functionality to vanilla
    Still bug where picking up items doesn't get logged but issue exists in vanilla too. Interacting with the item in your inventory logs it though

--- src/main/java/turniplabs/halplibe/mixin/mixins/registry/BlockMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/registry/BlockMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.core.block.Block;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import turniplabs.halplibe.helper.BlockHelper;
 
@@ -16,5 +17,8 @@ public class BlockMixin {
 	@Inject(at = @At("TAIL"), method = "<clinit>")
 	private static void captureHighest(CallbackInfo ci) {
 		BlockHelper.highestVanilla = highestBlockId;
+	}
+	@Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/achievement/stat/StatList;onBlockInit()V"))
+	private static void delayInit() {
 	}
 }
--- src/main/java/turniplabs/halplibe/mixin/mixins/registry/MinecraftServerMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/registry/MinecraftServerMixin.java
@@ -1,15 +1,11 @@
 package turniplabs.halplibe.mixin.mixins.registry;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.achievement.stat.StatBase;
 import net.minecraft.core.achievement.stat.StatList;
-import net.minecraft.core.item.Item;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import turniplabs.halplibe.helper.BlockHelper;
 import turniplabs.halplibe.helper.RegistryHelper;
 
 import java.lang.reflect.Method;
@@ -23,8 +19,9 @@ public class MinecraftServerMixin {
 			m.setAccessible(true);
 			m.invoke(null);
 			m.setAccessible(false);
-			StatList.craftedItemStats = new StatBase[Item.itemsList.length];
-			StatList.pickUpItemStats = new StatBase[Item.itemsList.length];
+			StatList.init();
+			StatList.onBlockInit();
+			StatList.onItemInit();
 		} catch (Throwable err) {
 			throw new RuntimeException(err);
 		}
