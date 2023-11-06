02ac17093b693a0f5ddda8f9fd75a72f7dd0cee1
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-06 02:30:31 -0600

    Texture Atlas can be set to any arbitrary Number
    Texture Atlas can be set to any arbitrary Number
    
    Added Config file for changing the Atlas size
    
    Only Rescale when needed
    
    Cleanup

--- src/main/java/turniplabs/halplibe/mixin/mixins/RenderEngineMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/RenderEngineMixin.java
@@ -4,8 +4,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.option.enums.MipmapType;
 import net.minecraft.client.render.RenderEngine;
 import net.minecraft.client.render.dynamictexture.DynamicTexture;
+import net.minecraft.core.Global;
 import net.minecraft.core.util.helper.Buffer;
-import net.minecraft.core.util.helper.Textures;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,31 +13,22 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import turniplabs.halplibe.helper.TextureHelper;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.nio.ByteBuffer;
 import java.util.List;
-import java.util.Map;
 
 @Mixin(value = RenderEngine.class, remap = false)
 public abstract class RenderEngineMixin {
     @Shadow
     private List<DynamicTexture> dynamicTextures;
-
-    @Shadow public abstract int getTexture(String name);
-
     @Shadow protected abstract void generateMipmaps(ByteBuffer buffer, BufferedImage image, int levels, boolean smooth);
 
     @Shadow @Final public Minecraft minecraft;
     @Shadow private boolean clampTexture;
     @Shadow private boolean blurTexture;
-    @Shadow private Map<String, Integer> textureMap;
     @Unique
     private final RenderEngine thisAsRenderEngine = (RenderEngine)(Object)this;
     @Inject(method = "initDynamicTextures", at = @At("TAIL"))
@@ -53,20 +44,23 @@ public abstract class RenderEngineMixin {
     }
     @Inject(method = "setupTexture(Ljava/awt/image/BufferedImage;IZ)V", at = @At("HEAD"), cancellable = true)
     private void adjustAtlasSize(BufferedImage img, int id, boolean mipmap, CallbackInfo ci){
-        boolean doResize = false;
-        for (String key : textureMap.keySet()) {
-            if (key.equals("/terrain.png") || key.equals("/gui/items.png")) {
-                doResize = textureMap.get(key) == id;
-                if (doResize) {
-                    break;
-                }
+//        boolean doResize = false;
+//        for (String key : textureMap.keySet()) {
+//            if (key.equals("/terrain.png") || key.equals("/gui/items.png")) {
+//                doResize = textureMap.get(key) == id;
+//                if (doResize) {
+//                    break;
+//                }
+//            }
+//        }
+        if (mipmap){ // TODO Replace with a way to directly detect if a texture is Terrain.png or Items.png
+            BufferedImage resizedAtlas = new BufferedImage((img.getWidth()/32) * Global.TEXTURE_ATLAS_WIDTH_TILES, (img.getHeight()/32) * Global.TEXTURE_ATLAS_WIDTH_TILES, img.getType());
+            if (img.getWidth() == resizedAtlas.getWidth() && img.getHeight() == resizedAtlas.getHeight()){
+                return; // Don't scale and transfer image if they're the same size
             }
-        }
-        if (mipmap){
-            BufferedImage testImage = new BufferedImage(img.getWidth()*2, img.getHeight()*2, img.getType());
             for (int x = 0; x < img.getWidth(); x++) {
                 for (int y = 0; y < img.getHeight(); y++) {
-                    testImage.setRGB(x, y, img.getRGB(x, y));
+                    resizedAtlas.setRGB(x, y, img.getRGB(x, y));
                 }
             }
 
@@ -85,12 +79,12 @@ public abstract class RenderEngineMixin {
                 GL11.glTexParameteri(3553, 10242, 10497);
                 GL11.glTexParameteri(3553, 10243, 10497);
             }
-            int w = testImage.getWidth();
-            int h = testImage.getHeight();
-            Buffer.put(testImage);
+            int w = resizedAtlas.getWidth();
+            int h = resizedAtlas.getHeight();
+            Buffer.put(resizedAtlas);
             GL11.glTexImage2D(3553, 0, 6408, w, h, 0, 6408, 5121, Buffer.buffer);
             if (mipmap) {
-                this.generateMipmaps(Buffer.buffer, testImage, this.minecraft.gameSettings.mipmapLevels.value, this.minecraft.gameSettings.mipmapType.value == MipmapType.SMOOTH);
+                this.generateMipmaps(Buffer.buffer, resizedAtlas, this.minecraft.gameSettings.mipmapLevels.value, this.minecraft.gameSettings.mipmapType.value == MipmapType.SMOOTH);
             }
             ci.cancel();
         }
