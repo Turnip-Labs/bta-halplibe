d2a19cfda2fde6c8ccbbda5bfcb026dd86eac4b5
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-07 16:27:52 -0600

    Reset Server Modlist

--- src/main/java/turniplabs/halplibe/mixin/mixins/version/NetClientHandlerMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/version/NetClientHandlerMixin.java
@@ -2,12 +2,18 @@ package turniplabs.halplibe.mixin.mixins.version;
 
 import net.minecraft.client.net.handler.NetClientHandler;
 import net.minecraft.core.net.handler.NetHandler;
+import net.minecraft.core.net.packet.Packet2Handshake;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import turniplabs.halplibe.HalpLibe;
 import turniplabs.halplibe.helper.ModVersionHelper;
 import turniplabs.halplibe.util.version.IVersionPackets;
 import turniplabs.halplibe.util.version.PacketModList;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 @Mixin(value = NetClientHandler.class, remap = false)
@@ -22,5 +28,17 @@ public abstract class NetClientHandlerMixin extends NetHandler implements IVersi
         } catch (Throwable err) {
             err.printStackTrace();
         }
+    }
+    @Inject(method = "handleHandshake(Lnet/minecraft/core/net/packet/Packet2Handshake;)V", at = @At("HEAD"))
+    private void resetServerModlist(Packet2Handshake packet2handshake, CallbackInfo ci){
+        try {
+            Field m = ModVersionHelper.class.getDeclaredField("serverMods");
+            m.setAccessible(true);
+            m.set(null, null);
+            m.setAccessible(false);
+        } catch (Throwable err) {
+            err.printStackTrace();
+        }
+        HalpLibe.LOGGER.info(String.valueOf(ModVersionHelper.getServerModlist() == null));
     }
 }
