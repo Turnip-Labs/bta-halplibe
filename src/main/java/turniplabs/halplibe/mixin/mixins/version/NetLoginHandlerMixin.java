1ef2ac93409012c16ef68bfa5ecac4ac12a71636
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-12-15 21:03:17 -0600

    Fixes and Modlist packet option

--- src/main/java/turniplabs/halplibe/mixin/mixins/version/NetLoginHandlerMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/version/NetLoginHandlerMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import turniplabs.halplibe.HalpLibe;
 import turniplabs.halplibe.helper.ModVersionHelper;
 import turniplabs.halplibe.util.version.PacketModList;
 
@@ -15,6 +16,8 @@ import turniplabs.halplibe.util.version.PacketModList;
 public class NetLoginHandlerMixin {
     @Inject(method = "doLogin(Lnet/minecraft/core/net/packet/Packet1Login;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/net/handler/NetServerHandler;sendPacket(Lnet/minecraft/core/net/packet/Packet;)V", ordinal = 6, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
     private void modlistPacket(Packet1Login packet1login, CallbackInfo ci, EntityPlayerMP entityplayermp){
-        entityplayermp.playerNetServerHandler.sendPacket(new PacketModList(ModVersionHelper.getLocalModlist()));
+        if (HalpLibe.sendModlist){
+            entityplayermp.playerNetServerHandler.sendPacket(new PacketModList(ModVersionHelper.getLocalModlist()));
+        }
     }
 }
