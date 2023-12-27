906d366e91267e093bb181e38b336bb541cd8c13
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-12-27 12:40:38 -0600

    Removed redundant utility class

--- src/main/java/turniplabs/halplibe/mixin/mixins/ItemStackJsonAdapterMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/ItemStackJsonAdapterMixin.java
@@ -7,11 +7,9 @@ import net.minecraft.core.data.registry.recipe.adapter.ItemStackJsonAdapter;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import turniplabs.halplibe.util.ItemUtil;
 
 import java.lang.reflect.Type;
 
@@ -22,7 +20,7 @@ public class ItemStackJsonAdapterMixin {
     protected void deserializeKey(JsonElement json, Type typeOfT, JsonDeserializationContext context, CallbackInfoReturnable<ItemStack> cir) {
         JsonObject obj = json.getAsJsonObject();
         if (obj.has("key")) {
-            ItemStack stack = obj.has("amount") ? new ItemStack(ItemUtil.getItemFromKey(obj.get("key").getAsString()), obj.get("amount").getAsInt(), obj.get("meta").getAsInt()) : new ItemStack(ItemUtil.getItemFromKey(obj.get("key").getAsString()), 1, obj.get("meta").getAsInt());
+            ItemStack stack = obj.has("amount") ? new ItemStack(Item.nameToIdMap.get(obj.get("key").getAsString()), obj.get("amount").getAsInt(), obj.get("meta").getAsInt()) : new ItemStack(Item.nameToIdMap.get(obj.get("key").getAsString()), 1, obj.get("meta").getAsInt());
             cir.setReturnValue(stack);
         }
     }
