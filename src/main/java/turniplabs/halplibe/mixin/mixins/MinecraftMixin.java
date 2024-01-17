80e9a52f58e2f62769a901aaf5764694a9136d5c
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-16 18:58:12 -0600

    Remove testing recipes

--- src/main/java/turniplabs/halplibe/mixin/mixins/MinecraftMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/mixins/MinecraftMixin.java
@@ -26,41 +26,6 @@ public class MinecraftMixin {
     @Inject(method = "startGame", at = @At(value = "INVOKE",target = "Lnet/minecraft/core/data/DataLoader;loadRecipes(Ljava/lang/String;)V", ordinal = 3, shift = At.Shift.AFTER))
     public void recipeEntrypoint(CallbackInfo ci){
         FabricLoader.getInstance().getEntrypoints("recipesReady", RecipeEntrypoint.class).forEach(RecipeEntrypoint::onRecipesReady);
-        RecipeBuilder.Shapeless(HalpLibe.MOD_ID)
-                .addInput(Block.dirt)
-                .addInput(Block.cobweb)
-                .addInput(Block.algae)
-                .create("testShapeless", Block.blockDiamond.getDefaultStack());
-        RecipeBuilder.Shaped(HalpLibe.MOD_ID,
-                        "X X",
-                        " X ",
-                        "X X")
-                .addInput('X', Block.mobspawner)
-                .create("testShaped", Item.doorIron.getDefaultStack());
-
-        RecipeBuilderShaped stairsTemplate = RecipeBuilder.Shaped(HalpLibe.MOD_ID,
-                "X  ",
-                "XX ",
-                "XXX");
-        stairsTemplate.addInput('X', Block.dirt).create("stairs1", Block.stairsBrickLimestone.getDefaultStack());
-        stairsTemplate.addInput('X', Block.cobweb).create("stairs2", Block.blockDiamond.getDefaultStack());
-        stairsTemplate.addInput('X', Block.mesh).create("stairs3", Block.seat.getDefaultStack());
-        RecipeBuilder.ModifyTrommel("minecraft", "dirt")
-                .addEntry(new WeightedRandomLootObject(Item.diamond.getDefaultStack(), 1, 100), 20)
-                .addEntry(new WeightedRandomLootObject(Item.flag.getDefaultStack(), 5), 2)
-                .removeEntries(Item.ammoPebble.getDefaultStack());
-        RecipeBuilder.ModifyTrommel("minecraft", "clay")
-                .setWeights(Item.clay.getDefaultStack(), 200);
-        RecipeBuilder.Trommel(HalpLibe.MOD_ID)
-                .setInput(Block.cobbleBasalt)
-                .addEntry(new WeightedRandomLootObject(Item.olivine.getDefaultStack()), 1)
-                .create("cobbled_basalt");
-        RecipeBuilder.ModifyWorkbench("minecraft").removeRecipe("block_of_diamond").removeRecipe("block_of_diamond_to_diamond").removeRecipe("cake");
-        RecipeBuilder.ModifyBlastFurnace("minecraft").removeRecipe("coal_ores_to_coal");
-        RecipeBuilder.ModifyFurnace("minecraft").removeRecipe("porkchop_raw_to_porkchop_cooked");
-        if (HalpLibe.exportRecipes){
-            RecipeBuilder.exportRecipes();
-        }
     }
 
     @Inject(method = "startGame", at = @At("HEAD"))
