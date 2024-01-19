8468b250c80189fd780b569d28cb8f561ea63b80
azurelmao <de_arcane@protonmail.com>
2024-01-19 16:41:54 +0100

    Set remap to false

--- src/main/java/turniplabs/halplibe/mixin/accessors/WeightedRandomBagAccessor.java
+++ src/main/java/turniplabs/halplibe/mixin/accessors/WeightedRandomBagAccessor.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
 
-@Mixin(WeightedRandomBag.class)
+@Mixin(value = WeightedRandomBag.class, remap = false)
 public interface WeightedRandomBagAccessor {
     @Accessor("entries")
     List<WeightedRandomBag.Entry> getRawEntries();
--- src/main/java/turniplabs/halplibe/mixin/accessors/WeightedRandomBagEntryAccessor.java
+++ src/main/java/turniplabs/halplibe/mixin/accessors/WeightedRandomBagEntryAccessor.java
@@ -4,7 +4,7 @@ import net.minecraft.core.WeightedRandomBag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(WeightedRandomBag.Entry.class)
+@Mixin(value = WeightedRandomBag.Entry.class, remap = false)
 public interface WeightedRandomBagEntryAccessor {
     @Accessor("weight")
     void setWeight(double weight);
