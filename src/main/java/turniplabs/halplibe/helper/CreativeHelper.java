bd890dfa7331b109854f5481f45f63b866e3f69e
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-02-17 19:29:11 -0600

    java docs

--- src/main/java/turniplabs/halplibe/helper/CreativeHelper.java
+++ src/main/java/turniplabs/halplibe/helper/CreativeHelper.java
@@ -5,22 +5,57 @@ import net.minecraft.core.item.ItemStack;
 import turniplabs.halplibe.util.CreativeEntry;
 
 public class CreativeHelper {
-    public static void setPriority(IItemConvertible item, int priority){
-        setPriority(item.getDefaultStack(), priority);
+    /**
+     * @param itemToAdd The itemstack to be added to the creative inventory list
+     * @param priority the priority for the item to be added, lower numbers appear higher in the list. Default is 1000
+     */
+    @SuppressWarnings("unused")
+    public static void setPriority(IItemConvertible itemToAdd, int priority){
+        setPriority(itemToAdd.getDefaultStack(), priority);
     }
-    public static void setPriority(IItemConvertible item, int meta, int priority){
-        setPriority(new ItemStack(item, 1, meta), priority);
+
+    /**
+     * @param itemToAdd The itemstack to be added to the creative inventory list
+     * @param meta the meta value for the itemToAdd
+     * @param priority the priority for the item to be added, lower numbers appear higher in the list. Default is 1000
+     */
+    @SuppressWarnings("unused")
+    public static void setPriority(IItemConvertible itemToAdd, int meta, int priority){
+        setPriority(new ItemStack(itemToAdd, 1, meta), priority);
     }
-    public static void setPriority(ItemStack item, int priority){
-        CreativeEntry.addEntry(new CreativeEntry(item, priority));
+    /**
+     * @param itemToAdd The itemstack to be added to the creative inventory list
+     * @param priority the priority for the item to be added, lower numbers appear higher in the list. Default is 1000
+     */
+    @SuppressWarnings("unused")
+    public static void setPriority(ItemStack itemToAdd, int priority){
+        CreativeEntry.addEntry(new CreativeEntry(itemToAdd, priority));
     }
+
+    /**
+     * @param itemToAdd The itemstack to be added to the creative inventory list
+     * @param itemParent The itemstack that the itemToAdd will be placed after
+     */
+    @SuppressWarnings("unused")
     public static void setParent(IItemConvertible itemToAdd, IItemConvertible itemParent){
         setParent(itemToAdd.getDefaultStack(), itemParent.getDefaultStack());
     }
+    /**
+     * @param itemToAdd The itemstack to be added to the creative inventory list
+     * @param metaToAdd the meta value for the itemToAdd
+     * @param itemParent The itemstack that the itemToAdd will be placed after
+     * @param metaParent the meta value for the itemParent
+     */
+    @SuppressWarnings("unused")
     public static void setParent(IItemConvertible itemToAdd, int metaToAdd, IItemConvertible itemParent, int metaParent){
         setParent(new ItemStack(itemToAdd, 1, metaToAdd), new ItemStack(itemParent, metaParent));
     }
-    public static void setParent(ItemStack itemToAdd, ItemStack parentStack){
-        CreativeEntry.addEntry(new CreativeEntry(itemToAdd, parentStack));
+    /**
+     * @param itemToAdd The itemstack to be added to the creative inventory list
+     * @param itemParent The itemstack that the itemToAdd will be placed after
+     */
+    @SuppressWarnings("unused")
+    public static void setParent(ItemStack itemToAdd, ItemStack itemParent){
+        CreativeEntry.addEntry(new CreativeEntry(itemToAdd, itemParent));
     }
 }
