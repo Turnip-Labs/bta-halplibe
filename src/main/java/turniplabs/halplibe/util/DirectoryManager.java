535711975391c1307f8180c4672f793af2b7bd30
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-02-22 22:24:56 -0600

    Directory Manager improvement

--- src/main/java/turniplabs/halplibe/util/DirectoryManager.java
+++ src/main/java/turniplabs/halplibe/util/DirectoryManager.java
@@ -4,111 +4,71 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DirectoryManager {
     public static final HashMap<String, HashMap<String, String>> resourceDirectoryMap = new HashMap<>();
-    public static final String BLOCK_TEXTURES = "halplibe:block_textures";
-    public static final String ITEM_TEXTURES = "halplibe:item_textures";
-    public static final String LANGUAGE = "halplibe:language";
-    public static final String SOUND = "halplibe:sound";
-    public static final String MUSIC = "halplibe:music";
-    public static final String STREAMING = "halplibe:streaming";
-    public static final String CAVE_MUSIC = "halplibe:cave_music";
-    public static final String ARMOR = "halplibe:armor";
+    public static final List<String> allKeys = new ArrayList<>();
+    public static final String BLOCK_TEXTURES = registerKey("halplibe:block_textures");
+    public static final String ITEM_TEXTURES = registerKey("halplibe:item_textures");
+    public static final String PARTICLE_TEXTURES = registerKey("halplibe:particle_textures");
+    public static final String LANGUAGE = registerKey("halplibe:language");
+    public static final String SOUND = registerKey("halplibe:sound");
+    public static final String MUSIC = registerKey("halplibe:music");
+    public static final String STREAMING = registerKey("halplibe:streaming");
+    public static final String CAVE_MUSIC = registerKey("halplibe:cave_music");
+    public static final String ARMOR = registerKey("halplibe:armor");
     public static final FabricLoader fl = FabricLoader.getInstance();
     static {
         for (ModContainer container : fl.getAllMods()){
             String id = container.getMetadata().getId();
             ModMetadata metadata = container.getMetadata();
-            if (metadata.containsCustomValue(BLOCK_TEXTURES)){
-                assignBlockTextureDirectory(id, metadata.getCustomValue(BLOCK_TEXTURES).getAsString());
-            }
-            if (metadata.containsCustomValue(ITEM_TEXTURES)){
-                assignItemTextureDirectory(id, metadata.getCustomValue(ITEM_TEXTURES).getAsString());
-            }
-            if (metadata.containsCustomValue(LANGUAGE)){
-                assignLanguageDirectory(id, metadata.getCustomValue(LANGUAGE).getAsString());
-            }
-            if (metadata.containsCustomValue(MUSIC)){
-                assignMusicDirectory(id, metadata.getCustomValue(MUSIC).getAsString());
-            }
-            if (metadata.containsCustomValue(SOUND)){
-                assignSoundDirectory(id, metadata.getCustomValue(SOUND).getAsString());
-            }
-            if (metadata.containsCustomValue(CAVE_MUSIC)){
-                assignCaveMusicDirectory(id, metadata.getCustomValue(CAVE_MUSIC).getAsString());
-            }
-            if (metadata.containsCustomValue(STREAMING)){
-                assignStreamingDirectory(id, metadata.getCustomValue(STREAMING).getAsString());
-            }
-            if (metadata.containsCustomValue(ARMOR)){
-                assignArmorDirectory(id, metadata.getCustomValue(ARMOR).getAsString());
+            for (String key : allKeys){
+                if (metadata.containsCustomValue(key)){
+                    assignDirectory(key, id, metadata.getCustomValue(key).getAsString());
+                }
             }
         }
     }
-    public static void assignBlockTextureDirectory(String modID, String path){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(BLOCK_TEXTURES, path);
+    public static String registerKey(String key){
+        allKeys.add(key);
+        return key;
     }
-    public static void assignItemTextureDirectory(String modID, String path){
+    public static void assignDirectory(String key, String modID, String path){
         resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(ITEM_TEXTURES, path);
+        resourceDirectoryMap.get(modID).put(key, path);
     }
-    public static void assignLanguageDirectory(String modID, String path){
+    public static String getDirectory(String key, String modID, String formattedDefault){
         resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(LANGUAGE, path);
-    }
-    public static void assignSoundDirectory(String modID, String path){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(SOUND, path);
-    }
-    public static void assignMusicDirectory(String modID, String path){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(MUSIC, path);
-    }
-    public static void assignCaveMusicDirectory(String modID, String path){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(CAVE_MUSIC, path);
-    }
-    public static void assignStreamingDirectory(String modID, String path){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(STREAMING, path);
-    }
-    public static void assignArmorDirectory(String modID, String path){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        resourceDirectoryMap.get(modID).put(ARMOR, path);
+        return resourceDirectoryMap.get(modID).getOrDefault(key, String.format(formattedDefault, modID));
     }
     public static String getBlockTextureDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(BLOCK_TEXTURES, "/assets/" + modID + "/block/");
+        return getDirectory(BLOCK_TEXTURES, modID, "/assets/%s/block/");
     }
     public static String getItemTextureDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(ITEM_TEXTURES, "/assets/" + modID + "/item/");
+        return getDirectory(ITEM_TEXTURES, modID, "/assets/%s/item/");
+    }
+    public static String getParticleTextureDirectory(String modID){
+        return getDirectory(PARTICLE_TEXTURES, modID, "/assets/%s/particle/");
     }
     public static String getLanguageDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(LANGUAGE, "/lang/" + modID + "/");
+        return getDirectory(LANGUAGE, modID, "/lang/%s/");
     }
     public static String getSoundDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(SOUND, "/assets/" + modID + "/sound/");
+        return getDirectory(SOUND, modID, "/assets/%s/sound/");
     }
     public static String getMusicDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(MUSIC, "/assets/" + modID + "/music/");
+        return getDirectory(MUSIC, modID, "/assets/%s/music/");
     }
     public static String getCaveMusicDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(CAVE_MUSIC, "/assets/" + modID + "/cavemusic/");
+        return getDirectory(CAVE_MUSIC, modID, "/assets/%s/cavemusic/");
     }
     public static String getStreamingDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(STREAMING, "/assets/" + modID + "/streaming/");
+        return getDirectory(STREAMING, modID, "/assets/%s/streaming/");
     }
     public static String getArmorDirectory(String modID){
-        resourceDirectoryMap.putIfAbsent(modID, new HashMap<>());
-        return resourceDirectoryMap.get(modID).getOrDefault(ARMOR, "/assets/" + modID + "/armor/");
+        return getDirectory(ARMOR, modID, "/assets/%s/armor/");
     }
 }
