5c935f3c77dcaa666284c32ad8f854aa79bcdc23
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-06-20 18:32:58 +0200

    Fix issues with instances not being available in dispatcher mixins.

--- src/main/java/turniplabs/halplibe/mixin/models/BlockModelDispatcherMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/models/BlockModelDispatcherMixin.java
@@ -2,8 +2,10 @@ package turniplabs.halplibe.mixin.models;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.block.model.BlockModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModelDispatcher;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,14 +15,14 @@ import turniplabs.halplibe.util.ModelEntrypoint;
 @Mixin(value = BlockModelDispatcher.class)
 public abstract class BlockModelDispatcherMixin {
 
-    @Shadow
-    private static BlockModelDispatcher instance;
+    @Unique
+    public BlockModelDispatcher thisAs = (BlockModelDispatcher) (Object)this;
 
     @Inject(method = "reload", at = @At("TAIL"))
     private void addQueuedModels(CallbackInfo ci) {
         FabricLoader.getInstance()
                 .getEntrypoints("initModels", ModelEntrypoint.class)
-                .forEach(e -> e.initBlockModels(instance));
-        ClientEvents.BLOCK_MODEL_RELOAD.emit(consumer -> consumer.accept(instance));
+                .forEach(e -> e.initBlockModels(thisAs));
+        ClientEvents.BLOCK_MODEL_RELOAD.emit(consumer -> consumer.accept(thisAs));
     }
 }
