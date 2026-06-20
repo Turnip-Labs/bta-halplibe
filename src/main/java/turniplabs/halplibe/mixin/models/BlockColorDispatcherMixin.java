4d6de19d1b819956b8e9717c195826e435c319ef
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-06-20 18:32:58 +0200

    Fix issues with instances not being available in dispatcher mixins.

--- src/main/java/turniplabs/halplibe/mixin/models/BlockColorDispatcherMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/models/BlockColorDispatcherMixin.java
@@ -3,11 +3,13 @@ package turniplabs.halplibe.mixin.models;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.block.color.BlockColor;
 import net.minecraft.client.render.block.color.BlockColorDispatcher;
+import net.minecraft.client.render.block.model.BlockModelDispatcher;
 import net.minecraft.client.util.dispatch.Dispatcher;
 import net.minecraft.core.block.Block;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,15 +19,14 @@ import turniplabs.halplibe.util.ModelEntrypoint;
 @Mixin(value = BlockColorDispatcher.class)
 public abstract class BlockColorDispatcherMixin extends Dispatcher<Block<?>, BlockColor> {
 
-    @Shadow
-    @Final
-    private static BlockColorDispatcher instance;
+    @Unique
+    public BlockColorDispatcher thisAs = (BlockColorDispatcher) (Object)this;
 
     @Inject(method = "reload", at = @At("TAIL"))
     private void addQueuedModels(CallbackInfo ci) {
         FabricLoader.getInstance()
                 .getEntrypoints("initModels", ModelEntrypoint.class)
-                .forEach(e -> e.initBlockColors(instance));
-        ClientEvents.BLOCK_COLOR_RELOAD.emit(consumer -> consumer.accept(instance));
+                .forEach(e -> e.initBlockColors(thisAs));
+        ClientEvents.BLOCK_COLOR_RELOAD.emit(consumer -> consumer.accept(thisAs));
     }
 }
--- src/main/java/turniplabs/halplibe/mixin/models/TileEntityRendererDispatcherMixin.java
+++ src/main/java/turniplabs/halplibe/mixin/models/TileEntityRendererDispatcherMixin.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,16 +16,14 @@ import turniplabs.halplibe.util.ModelEntrypoint;
 @Mixin(value = TileEntityRenderDispatcher.class)
 public abstract class TileEntityRendererDispatcherMixin {
 
-    @Shadow
-    @Final
-    @NotNull
-    public static TileEntityRenderDispatcher instance;
+    @Unique
+    public TileEntityRenderDispatcher thisAs = (TileEntityRenderDispatcher) (Object)this;
 
     @Inject(method = "reload", at = @At(value = "TAIL"))
     private void addQueuedModels(CallbackInfo ci) {
         FabricLoader.getInstance()
                 .getEntrypoints("initModels", ModelEntrypoint.class)
-                .forEach(e -> e.initTileEntityModels(instance));
-        ClientEvents.TILE_ENTITY_RENDERER_RELOAD.emit(consumer -> consumer.accept(instance));
+                .forEach(e -> e.initTileEntityModels(thisAs));
+        ClientEvents.TILE_ENTITY_RENDERER_RELOAD.emit(consumer -> consumer.accept(thisAs));
     }
 }
