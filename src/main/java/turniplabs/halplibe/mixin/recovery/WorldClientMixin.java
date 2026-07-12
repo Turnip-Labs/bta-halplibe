2fa80cb6e8c9ff9f1a3b6f57dc456bfd7c7db545
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-07-12 16:44:47 +0200

    Why was this separated again?

---
+++ src/main/java/turniplabs/halplibe/mixin/recovery/WorldClientMixin.java
@@ -0,0 +1,19 @@
+package turniplabs.halplibe.mixin.recovery;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.client.world.WorldClient;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import turniplabs.halplibe.HalpLibe;
+
+@Mixin(value = WorldClient.class,remap = false)
+public abstract class WorldClientMixin {
+
+    @WrapWithCondition(method = "updateTileEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/block/entity/TileEntity;tick()V"))
+    public boolean disableTickingTileEntities(TileEntity instance) {
+        return !HalpLibe.CONFIG.getBoolean("recoveryMode");
+    }
+
+}
